9058d98c8c05ee8800e5db028b5baf19d12a6597
German Cruz <cruzgerman216@yahoo.com>
2020-03-03 17:25:07 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  arr = []
+  array.each do |element|
+    arr.push(badge_maker(element))
+  end
+  return arr
+end
+
+def assign_rooms(array)
+  arr = []
+  array.each_with_index { |item, index|
+    arr.push("Hello, #{item}! You'll be assigned to room #{index+1}!")
+  }
+  return arr
+end
+
+def printer(array)
+  getbatch = batch_badge_creator(array)
+
+  getbatch.each do |element|
+    puts element
+  end
+
+  getassign = assign_rooms(array)
+  getassign.each do |element|
+    puts element
+  end
+end
