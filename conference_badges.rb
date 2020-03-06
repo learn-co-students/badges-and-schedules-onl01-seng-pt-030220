3cfec4865b99f5e7944b02c971a1e9aeab5395eb
Megan Christine McClure <mcclme02@luther.edu>
2020-03-06 18:59:19 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,43 @@
 # Write your code here.
+#inputs--> name --> String
+#outputs--> "Hellos, my name is Arel." --> String
+
+def badge_maker (name)
+  return "Hello, my name is #{name}."
+end
+
+#inputs--> list of attendees --> Array of strings
+#outputs--> badge messages --> Array of strings
+
+def batch_badge_creator(attendees)
+  attendees.collect do |name| 
+    badge_maker(name)
+  end
+end
+
+#inputs-->list of attendees-->array of strings #accessed by its index number in the array
+      #-->rooms 1-7 --> integers (or string?)
+#outputs-->list of room assignments, "Hello, attendee! You'll be assigned to room ___!"-->string inside an array
+
+def assign_rooms(attendees)
+  room = 0 
+  attendees.collect do |name|
+    room += 1 
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
+#inputs-->batch_badge_creator-->method 
+     # -->assign_rooms-->method 
+#outputs-->none 
+#actions-->puts batch_badge_creator-->string
+       #-->puts assign_rooms--> string 
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |print|
+    puts print
+  end
+  assign_rooms(attendees).each do |print|
+    puts print
+  end
+end
